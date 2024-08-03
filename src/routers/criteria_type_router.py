67e84a027f56cae848097e7fe95a93b0a43ea3be
OkuSan - Paratpanu Pechsaman <paratpanu18@gmail.com>
2024-08-04 01:47:23 +0700

    hotfix - Fix criteria type bug

--- src/routers/criteria_type_router.py
+++ src/routers/criteria_type_router.py
@@ -4,10 +4,10 @@ from src.schemas import CriteriaTypeDTO
 
 criteria_type_router = APIRouter(tags=["criteriaType"])
 
-@criteria_type_router.post('/')
+@criteria_type_router.post('/criteriaType')
 def create(criteria_type: CriteriaTypeDTO) -> dict:
     return CriteriaTypeService.create(criteria_type)
 
-@criteria_type_router.get('/')
-def get_all() -> dict:
+@criteria_type_router.get('/criteriaType')
+def get_all() -> list[dict]:
     return CriteriaTypeService.get_all()
--- src/services/criteria_type_service.py
+++ src/services/criteria_type_service.py
@@ -3,12 +3,17 @@ from src.db import criteria_type_collection
 
 class CriteriaTypeService:
     def create(data: CriteriaTypeDTO) -> dict:
-        name = data['name']
+        name = data.name
         criteria_type_collection.insert_one({
             'name': name
         })
         return {'name': name}
     
     def get_all() ->dict:
-        all_criteria_type = criteria_type_collection.find()
-        return all_criteria_type
+        all_criteria_types = []
+        for criteria_type in criteria_type_collection.find():
+            all_criteria_types.append({
+                'id': str(criteria_type['_id']),
+                'name': criteria_type['name']
+            })
+        return all_criteria_types
