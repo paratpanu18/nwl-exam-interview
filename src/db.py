c8096884918c2729bc2cdad29bbe1a899810a303
OkuSan - Paratpanu Pechsaman <paratpanu18@gmail.com>
2024-08-04 00:27:58 +0700

    Initial Commit

---
+++ src/db.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+from src.settings import Settings
+
+SETTINGS = Settings()
+
+client = MongoClient(SETTINGS.MONGO_CONNECTION_STRING)
+db = client[SETTINGS.MONGO_DB_NAME]
